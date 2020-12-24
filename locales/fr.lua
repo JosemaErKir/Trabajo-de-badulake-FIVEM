d57963b289e4f8e7bc2fc235abfddbeedfdbe4ad
JosemaErKir <68438454+JosemaErKir@users.noreply.github.com>
2020-12-24 23:44:31 +0100

    Add files via upload

---
+++ locales/fr.lua
@@ -0,0 +1,110 @@
+Locales['fr'] = {
+    -- Cloakroom
+    ['cloakroom']                = 'Vestimenta',
+    ['citizen_wear']             = 'Vestido civil',
+    ['barman_outfit']            = 'Badulaque',
+    ['dancer_outfit_1']          = 'Traje de baile 1',
+    ['dancer_outfit_2']          = 'Traje de baile 2',
+    ['dancer_outfit_3']          = 'Traje de baile 3',
+    ['dancer_outfit_4']          = 'Traje de baile 4',
+    ['dancer_outfit_5']          = 'Traje de baile 5',
+    ['dancer_outfit_6']          = 'Traje de baile 6',
+    ['dancer_outfit_7']          = 'Traje de baile 7',
+    ['no_outfit']                = 'No hay uniforme a tu medida ...',
+    ['open_cloackroom']          = 'Presione ~INPUT_CONTEXT~ para cambiar',
+
+    -- Vault  
+    ['get_weapon']               = 'Tomar arma',
+    ['put_weapon']               = 'Depositar arma',
+    ['get_weapon_menu']          = 'Seguro: toma el arma',
+    ['put_weapon_menu']          = 'Caja fuerte - Depósito de armas',
+    ['get_object']               = 'Tomar objeto',
+    ['put_object']               = 'Depositar objeto',
+    ['vault']                    = 'armario',
+    ['open_vault']               = 'Presione ~INPUT_CONTEXT~ para acceder a la armario',
+
+    -- Despensa
+    ['get_object']               = 'Tomar objeto',
+    ['put_object']               = 'Depositar objeto',
+    ['fridge']                   = 'Despensa',
+    ['open_fridge']              = 'Presione ~INPUT_CONTEXT~ para acceder a la despensa',
+    ['badulake_fridge_stock']     = 'Contenido de la despensa',
+    ['fridge_inventory']         = 'Depositar a la despensa',
+
+    -- Shops  
+    ['shop']                     = 'Tienda',
+    ['shop_menu']                = 'Presione ~INPUT_CONTEXT~ para acceder a la tienda.',
+    ['bought']                   = 'Has comprado ~b~1x ',
+    ['not_enough']         = 'No tienes suficiente dinero.',
+    ['max_item']                 = 'Ya estás cargando lo suficiente contigo mismo.',
+    ['Coca-cola']				= 'Coca-Cola',
+    ['bread']				= 'Pan',
+    ['Cafe']                   = 'Cafe',
+    ['Sprite']                   = 'Sprite',
+    ['Fanta']                   = 'Fanta',
+    ['Hamburguesa']                   = 'Hamburguesa',
+	['Leche']					= 'Leche',
+	['Agua']					= 'Agua',
+	['Limonada']				= 'Limonada',
+	['bolsacahuetes']			= 'Bolsa Cacahuetes',
+	['Salchichón']				= 'Salchichón',
+	['macka']						= 'macka',
+	['Chocolate']				= 'Chocolate',
+    ['Magdalena']				= 'Magdalena',
+    ['pastacarbonara']				= 'Pasta Carbonara',
+    ['Monster']				= 'Monster',
+    ['Patatas']				= 'Patatas',
+    ['Mechero']				= 'Mechero',
+    ['Pizza']               = 'Pizza',
+    ['Galletas']               = 'Galletas',
+    ['Helado']               = 'Helado',
+    ['cocidoabuela']               = 'Cocida de la abuela',
+    ['Donut']					= 'Donut',
+    ['cigarett']                = 'Cigarro',
+    ['menta']					= 'Menta',
+
+
+    -- Vehicles  
+    ['vehicle_menu']             = 'Vehículo',
+    ['vehicle_out']              = 'Ya hay un vehículo afuera',
+    ['vehicle_spawner']          = 'Presione ~INPUT_CONTEXT~ para salir de un vehículo',
+    ['store_vehicle']            = 'Presione ~INPUT_CONTEXT~ para almacenar el vehículo',
+    ['service_max']              = 'Servicio completo: ',
+    ['spawn_point_busy']         = 'Un vehículo ocupa el punto de salida.',
+
+    -- Boss Menu  
+    ['take_company_money']       = 'Retirar el dinero del negocio',
+    ['deposit_money']            = 'Depositar dinero',
+    ['amount_of_withdrawal']     = 'Cantidad de retiro',
+    ['invalid_amount']           = 'Cantidad inválida',
+    ['amount_of_deposit']        = 'Cantidad del deposito',
+    ['open_bossmenu']            = 'Presione ~INPUT_CONTEXT~ para abrir el menú',
+    ['invalid_quantity']         = 'Cantidad inválida',
+    ['you_removed']              = 'Eliminaste x',
+    ['you_added']                = 'Agregaste x',
+    ['quantity']                 = 'Cantidad',
+    ['inventory']                = 'Inventario',
+    ['bahama_mamas_stock']       = 'Stock de badulake',
+    
+
+    -- Billing Menu  
+    ['badulake'] = 'Menu Badulake',
+    ['billing'] = 'Facturas',
+    ['billing_amount'] = 'Cantidad',
+    -- Crafting Menu  
+
+
+    -- Misc  
+
+
+    -- Phone  
+    ['bahama_mamas_phone']            = 'Bahama Mamas',
+    ['bahama_mamas_customer']         = 'Ciudadano',
+
+    -- Teleporters
+    ['e_to_enter_1']             = 'Presione ~INPUT_PICKUP~ para ir detrás de la barra',
+    ['e_to_exit_1']              = 'Presione ~INPUT_PICKUP~ para pasar la barra.',
+    ['e_to_enter_2']             = 'Presione ~INPUT_PICKUP~ para subir al techo.',
+    ['e_to_exit_2']              = 'Presione ~INPUT_PICKUP~ para ir a los escritorios.',
+    
+}
